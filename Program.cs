a40766c26c9c964225c8f21b8806a45c45106914
johnjoker13 <joaoavelino2008@hotmail.com>
2022-10-07 14:21:16 -0300

    feat: adds sqlite provider

--- Program.cs
+++ Program.cs
@@ -3,9 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using EmployeesAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Employees") ?? "Data Source=Employees.db";
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<EmployeeDB>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<EmployeeDB>(connectionString);
 builder.Services.AddSwaggerGen(c =>
 {
      c.SwaggerDoc("v1", new OpenApiInfo {
